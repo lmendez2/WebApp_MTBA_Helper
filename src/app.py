95a6d8663cc73bf6bb8d429352048f8956a3001f
lmendez2 <lmendez2@babson.edu>
2017-12-01 22:22:30 -0500

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,16 +1,25 @@
 """
 Simple "Hello, World" application using Flask
 """
+from flask import Flask, render_template, request, redirect, url_for
+import math
+from mbta_helper import find_stop_near
 
-from flask import Flask
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/',methods=['GET','POST'])
+def find_stop():
+    if request.method == 'POST':
+        place = request.form['userloc']
+        stop, distance = find_stop_near(place)
+        print(stop)
+        print(distance)
+        return render_template('results.html', stop = stop, distance=distance)
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host ='127.0.0.1', port=5000)
--- src/mbta_helper.py
+++ src/mbta_helper.py
@@ -1,11 +1,15 @@
 import urllib.request   # urlencode function
 import json
 
+from pprint import pprint
+
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
 GMAPS_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 MBTA_BASE_URL = "http://realtime.mbta.com/developer/api/v2/stopsbylocation"
 MBTA_DEMO_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
+
+url = "https://maps.googleapis.com/maps/api/geocode/json?address=231+Forest+Street,+Wellesley,+MA+02457,+USA&key=AIzaSyDA1865acKD_jP_65hIQ24lo1LmUjHiRHY"
 
 
 # A little bit of scaffolding if you want to use it
@@ -15,8 +19,10 @@ def get_json(url):
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
-
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
 
 def get_lat_long(place_name):
     """
@@ -25,8 +31,22 @@ def get_lat_long(place_name):
     See https://developers.google.com/maps/documentation/geocoding/
     for Google Maps Geocode API URL formatting requirements.
     """
-    pass
 
+    if ' ' in place_name:
+        while ' ' in place_name:
+            x = []
+            for i in place_name:
+                x.append(i)
+            while ' ' in x:
+                x[x.index(' ')] = '%20'
+            place_name = ''.join(x)
+
+    url = GMAPS_BASE_URL + '?address=' + place_name
+    place_json = get_json(url)
+    lat = place_json['results'][0]['geometry']['location']['lat']
+    lon = place_json['results'][0]['geometry']['location']['lng']
+    return (lat, lon)
+        
 
 def get_nearest_station(latitude, longitude):
     """
@@ -35,12 +55,34 @@ def get_nearest_station(latitude, longitude):
     See http://realtime.mbta.com/Portal/Home/Documents for URL
     formatting requirements for the 'stopsbylocation' API in 'MBTA-realtime API v2 Documentation'.
     """
-    pass
+    MBTA_url = MBTA_BASE_URL + '?api_key=wX9NwuHnZU2ToO7GmGR9uw&lat='+str(latitude)+'&lon='+str(longitude)+'&format=json'
+    response_data_mbta = get_json(MBTA_url)
 
+    if response_data_mbta['stop'] != []:
+        x = 0
+        station_name = response_data_mbta['stop'][x]['parent_station_name']
+        distance = response_data_mbta['stop'][x]['distance']
+        while station_name == '':
+            x += 1
+            station_name = response_data_mbta['stop'][x]['parent_station_name']
+            distance = response_data_mbta['stop'][x]['distance']
+        return station_name, distance
 
 def find_stop_near(place_name):
     """
     Given a place name or address, return the nearest MBTA stop and the 
     distance from the given place to that stop.
     """
-    pass
+    lat, lon = get_lat_long(place_name)
+    return get_nearest_station(lat, lon)
+
+
+
+def main():
+    place = input()
+
+    print(find_stop_near(place))
+
+
+if __name__ == '__main__':
+    main()
